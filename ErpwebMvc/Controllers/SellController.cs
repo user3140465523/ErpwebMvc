ed6d1582dff785e040f51fb1762b77f65b3fcf8d
冯金锟 <@>
2020-07-01 15:08:37 +0800

    123

---
+++ ErpwebMvc/Controllers/SellController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ErpwebMvc.Controllers
+{
+    public class SellController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //销量
+        public ActionResult Faction()
+        {
+            return View();
+        }
+        //显示商品信息
+        public ActionResult ShowGoods()
+        {
+            return View();
+        }
+        //申请加薪
+        public ActionResult AddMoney()
+        {
+            return View();
+        }
+        //评级 
+        public ActionResult Assess()
+        {
+            return View();
+        }
+         
+        //主页
+        public ActionResult SellShow()
+        {
+            return View();
+        }
+
+    }
+}
